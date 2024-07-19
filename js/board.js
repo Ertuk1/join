23349e5c7d211017e16929fde5102449f9c9f804
ertu1k <e.kurtulmus@live.com>
2024-07-19 15:21:03 +0200

    check if content is empty and add no task

--- js/board.js
+++ js/board.js
@@ -1,3 +1,24 @@
 async function initBoard() {
     await includeHTML();
+    checkIfEmpty();
+}
+
+function checkIfEmpty() {
+    let toDo = document.getElementById('toDo');
+    let progress = document.getElementById('progress');
+    let feedback = document.getElementById('feedback');
+    let done = document.getElementById('done');
+
+    if (progress.innerHTML.trim() === "") {
+        progress.innerHTML = /*html*/`<div class="noTasks"><span class="noTaskText">Nothing in progress</span></div>`;
+    }
+    if (toDo.innerHTML.trim() === "") {
+        toDo.innerHTML = /*html*/`<div class="noTasks"><span class="noTaskText">No tasks To do</span></div>`;
+    }
+    if (feedback.innerHTML.trim() === "") {
+        feedback.innerHTML = /*html*/`<div class="noTasks"><span class="noTaskText">No tasks awaiting feedback</span></div>`;
+    }
+    if (done.innerHTML.trim() === "") {
+        done.innerHTML = /*html*/`<div class="noTasks"><span class="noTaskText">No tasks done</span></div>`;
+    }
 }
