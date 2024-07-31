9bd0a52516d961a6a4c594b6fdd164646d83f23f
Julia Schäffer <juliaschaeffer96@gmail.com>
2024-07-31 14:23:57 +0200

    clean code

--- js/contacts.js
+++ js/contacts.js
@@ -58,10 +58,8 @@ let beautifulColors = [
 
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-
 async function contactInit() {
     await includeHTML();
-    currentPage();
     renderContacts();
 }
 
@@ -75,61 +73,60 @@ function createContactList() {
     contactList.innerHTML = '';
     let currentLetter = null;
     const seenContacts = new Set(); // Set zum Nachverfolgen der bereits hinzugefügten Kontakte
-  
+
     for (let j = 0; j < alphabet.length; j++) {
-      const letter = alphabet[j];
-      let letterAdded = false; // Flag, um zu überprüfen, ob der Buchstabe hinzugefügt wurde
-  
-      for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        const initials = contact.initialien;
-        const firstLetter = initials.charAt(0).toUpperCase();
-  
-        if (firstLetter === letter && !seenContacts.has(contact.name)) {
-          if (!letterAdded) {
-            const letterHeading = document.createElement('div');
-            letterHeading.textContent = firstLetter;
-            letterHeading.classList.add('letter-heading');
-            contactList.appendChild(letterHeading);
-            letterAdded = true;
-          }
-  
-          const contactItem = document.createElement('div');
-          contactItem.classList.add('contact');
-          const profileColor = contact['profileColor'];
-          const profilePicture = document.createElement('div');
-          profilePicture.classList.add('profile-picture');
-          profilePicture.style.backgroundColor = profileColor;
-          profilePicture.textContent = initials;
-          contactItem.appendChild(profilePicture);
-  
-          const contactDetails = document.createElement('div');
-          contactDetails.classList.add('oneContact');
-          contactDetails.innerHTML = `
+        const letter = alphabet[j];
+        let letterAdded = false; // Flag, um zu überprüfen, ob der Buchstabe hinzugefügt wurde
+
+        for (let i = 0; i < contacts.length; i++) {
+            const contact = contacts[i];
+            const initials = contact.initialien;
+            const firstLetter = initials.charAt(0).toUpperCase();
+
+            if (firstLetter === letter && !seenContacts.has(contact.name)) {
+                if (!letterAdded) {
+                    const letterHeading = document.createElement('div');
+                    letterHeading.textContent = firstLetter;
+                    letterHeading.classList.add('letter-heading');
+                    contactList.appendChild(letterHeading);
+                    letterAdded = true;
+                }
+
+                const contactItem = document.createElement('div');
+                contactItem.classList.add('contact');
+                const profileColor = contact['profileColor'];
+                const profilePicture = document.createElement('div');
+                profilePicture.classList.add('profile-picture');
+                profilePicture.style.backgroundColor = profileColor;
+                profilePicture.textContent = initials;
+                contactItem.appendChild(profilePicture);
+
+                const contactDetails = document.createElement('div');
+                contactDetails.classList.add('oneContact');
+                contactDetails.innerHTML = `
                       <h2>${contact.name}</h2>
                       <p class="blueColor">${contact.email}</p>
                   `;
-          contactItem.appendChild(contactDetails);
-          contactList.appendChild(contactItem);
-  
-          // Füge dem Kontakt und den Kontaktinformationen einen Click-Event-Listener hinzu
-          contactItem.addEventListener('click', handleClick);
-          function handleClick(event) {
-            // Stelle sicher, dass nur das geklickte Element behandelt wird
-            if (event.target === contactItem || event.target.parentElement === contactDetails) {
-              // Rufe die Kontaktinformationen mit dem aktuellen Kontakt ab
-              contactClickHandler(contact, i);
-            }
-          }
-  
-          seenContacts.add(contact.name); // Kontakt als gesehen markieren
-        }
-      }
-    }
-  }
+                contactItem.appendChild(contactDetails);
+                contactList.appendChild(contactItem);
 
-  function showAddContact() {
+                // Füge dem Kontakt und den Kontaktinformationen einen Click-Event-Listener hinzu
+                contactItem.addEventListener('click', handleClick);
+                function handleClick(event) {
+                    // Stelle sicher, dass nur das geklickte Element behandelt wird
+                    if (event.target === contactItem || event.target.parentElement === contactDetails) {
+                        // Rufe die Kontaktinformationen mit dem aktuellen Kontakt ab
+                        contactClickHandler(contact, i);
+                    }
+                }
+                seenContacts.add(contact.name); // Kontakt als gesehen markieren
+            }
+        }
+    }
+}
+
+function showAddContact() {
     document.getElementById('blurBackground').classList.remove('d-none');
     document.getElementById('buttonActiveImg').classList.add('buttonActiveImg');
     document.getElementById('addContactButton').classList.add('buttonActive');
-  }
+}
