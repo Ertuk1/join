773672811d3e0bb236a8e108f4f4b93c33c3ce9e
Adrian Krynicki <adrian.krynicki88@gmail.com>
2024-08-02 22:36:25 +0200

    small fixes

---
+++ js/signUp.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://join-323f5-default-rtdb..firebasedatabase.app/';
+
+function initSignUp() {
+    fetchData();
+}
+
+function redirectToLogIn() {
+    window.location.href = "index.html";
+  }
