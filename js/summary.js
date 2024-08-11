44218f2503420fe1f83c086632b271054a50136b
Adrian Krynicki <adrian.krynicki88@gmail.com>
2024-08-11 22:44:56 +0200

    Update Summary

--- js/summary.js
+++ js/summary.js
@@ -2,9 +2,11 @@ let newUser = JSON.parse(sessionStorage.getItem('currentUser'));
 
 async function summeryInit() {
     await includeHTML();
+    await loadDataTask();
     showInitials();
     updateGreeting();
     userName();
+    updateSummary();
 }
 
 function redirectToBoard() {
@@ -32,5 +34,32 @@ function userName() {
     ${currentUser}
   `
 }
+
+function updateSummary() {
+  const tasksInBoard = tasks.length;
+  const tasksInProgress = tasks.filter(task => task.status === 'progress').length;
+  const awaitingFeedback = tasks.filter(task => task.status === 'feedback').length;
+  const urgent = tasks.filter(task => task.prio === 'urgent').length;
+  const urgentTasks = tasks.filter(task => task.prio === 'urgent');
+  const sortedUrgentTasks = urgentTasks.sort((a, b) => new Date(a.date) - new Date(b.date));
+  const upcomingDeadline = sortedUrgentTasks.length > 0 ? formatDate(sortedUrgentTasks[0].date) : 'No urgent tasks';
+  const done = tasks.filter(task => task.status === 'done').length;
+  const toDo = tasks.filter(task => task.status === 'toDo').length;
+
+  document.getElementById('tasksInBoard').textContent = tasksInBoard;
+  document.getElementById('tasksInProgress').textContent = tasksInProgress;
+  document.getElementById('awaitingFeedback').textContent = awaitingFeedback;
+  document.getElementById('urgent').textContent = urgent;
+  document.getElementById('upcomingDeadline').textContent = upcomingDeadline;
+  document.getElementById('done').textContent = done;
+  document.getElementById('toDo').textContent = toDo;
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { month: 'long', day: 'numeric', year: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
 
   
